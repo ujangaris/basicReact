578e7295fee89efa423d40fe356f21deaceae667
ujangaja <ujangaja@gmail.com>
2021-08-30 17:15:58 +0700

    #3 state pada react | timer sederhana

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-//Komponent - Properties
-function Biodata(props){
-  return <span>umurnya{props.age} </span>
+//STATE
+class Timer extends Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      time : props.start
+    }
+  }
+
+  //LifeCycle
+  componentDidMount(){
+      this.addInterval = setInterval(()=> this.increase(),1000)  
+  }
+  componentWillUnmount(){
+    clearInterval(this.addInterval)
+  }
+
+  increase(){
+    //update state time setiap detik
+    this.setState((state,props) => ({
+      time: parseInt(this.state.time) + 1
+    }))
+  }
+  
+  render(){
+    return(
+      <div>{this.state.time} Detik</div>
+    )
+  }
 }
 
-function Greeting(props){
-  return <h1>Halo {props.name} - <Biodata age={props.age} /> </h1>
-}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Greeting name="Ujang" age="25" />
-        <Greeting name="Aris" age="20"/>
-        <Greeting name="Danu" age="22"/>
+        <Timer start="0" />
+        <Timer start="5" />
       </header>
     </div>
   );
