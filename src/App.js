030cfbbdeead686f483d195a5baf1a5f4223ddac
ujangaja <ujangaja@gmail.com>
2021-08-30 16:44:21 +0700

    #2 component dan props di react |mengunakan properti/props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Greeting(){
-  return <h1>Halo Nama</h1>
+function Greeting(props){
+  return <h1>Halo {props.name}</h1>
 }
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Greeting/>
+        <Greeting name="Ujang"/>
+        <Greeting name="Aris"/>
+        <Greeting name="Danu"/>
       </header>
     </div>
   );
