01280a3e97c820247c5cab81b1dc31b1a63f3d8f
ujangaja <ujangaja@gmail.com>
2021-08-30 16:51:05 +0700

    #2 component dan props di react |mengoper data component di dalam component lain

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+//Komponent - Properties
+function Biodata(props){
+  return <span>umurnya{props.age} </span>
+}
+
 function Greeting(props){
-  return <h1>Halo {props.name}</h1>
+  return <h1>Halo {props.name} - <Biodata age={props.age} /> </h1>
 }
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Greeting name="Ujang"/>
-        <Greeting name="Aris"/>
-        <Greeting name="Danu"/>
+        <Greeting name="Ujang" age="25" />
+        <Greeting name="Aris" age="20"/>
+        <Greeting name="Danu" age="22"/>
       </header>
     </div>
   );
