4c6d4c877b3898f4bb89d6a934433babae617466
ujangaja <ujangaja@gmail.com>
2021-08-30 16:32:27 +0700

    Apa itu JSX | menulis dasar jsx pada react dengan html biasa

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+const name = "Sekolah Koding";
+const element = <h1>Halo {name} </h1>;
+ReactDOM.render(element, document.getElementById("master"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
