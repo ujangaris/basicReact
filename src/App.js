d6ae8657ff78d8d0ad72489afff7b5b5f5a36281
ujangaja <ujangaja@gmail.com>
2021-08-31 18:43:54 +0700

    #7 Konsumsi API dengan react

--- src/App.js
+++ src/App.js
@@ -2,37 +2,35 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 
-
 class App extends Component {
   constructor(props) {
-    super(props)
-  
+    super(props);
+
     this.state = {
-       items: [],
-       isLoading:true
+      items: [],
+      isLoading: true,
     };
-  };
-
-  componentDidMount(){
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(data =>this.setState({items:data, isLoading:false}))
   }
-  
-  
-  render() {
-    const {items, isLoading} = this.state
 
-    if(isLoading){
-      return <p>Loading...</p>
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => this.setState({ items: data, isLoading: false }));
+  }
+
+  render() {
+    const { items, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
     }
     return (
       <div>
-       <ul>
-         {items.map((item, index) =>
-          <li key={index}>{item.name}</li>
-          )}
-       </ul>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>{item.name}</li>
+          ))}
+        </ul>
       </div>
     );
   }
