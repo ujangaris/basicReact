89998e337785aecaa0579c378003c82c93a6e70d
ujangaja <ujangaja@gmail.com>
2021-09-01 14:21:55 +0700

    Halaman SPA dan routing dengan react router dom | halaman 404 dengan Switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 
-
-function Home(){
-  return <h2>Halaman Home</h2>
+function Home() {
+  return <h2>Halaman Home</h2>;
 }
-function ListView(){
-  return <h2>Semua User</h2>
+function ListView() {
+  return <h2>Semua User</h2>;
+}
+
+function NoMatch() {
+  return <h2>404, Halaman tidak ditemukan!</h2>;
 }
 
 class App extends Component {
@@ -17,12 +20,19 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <nav>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/users'>User</Link></li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/users">User</Link>
+            </li>
           </nav>
           <main>
-            <Route path='/' exact component={Home}/>
-            <Route path='/users' exact component={ListView}/>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/users" exact component={ListView} />
+              <Route component={NoMatch} />
+            </Switch>
           </main>
         </div>
       </BrowserRouter>
